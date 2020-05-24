469e3a6ce6cb77e347d90768a43074ad7c1bb44e
adamfairweather32@hotmail.com <adamfairweather32@hotmail.com>
2020-05-24 11:46:18 +0100

    Started adding in data table date editor

--- src/DataTable/components/editors/DataTableDateEditor.js
+++ src/DataTable/components/editors/DataTableDateEditor.js
@@ -1,17 +1,263 @@
 import React, { Component, forwardRef } from 'react';
+import { format } from 'date-fns';
+import InputMask from 'react-input-mask';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import EventIcon from '@material-ui/icons/Event';
+import IconButton from '@material-ui/core/IconButton';
+import Popover from '@material-ui/core/Popover';
+import { Calendar } from '@material-ui/pickers';
+import StyledFormControl from '../../styled/StyledFormControl';
 import StyledOutlinedInput from '../../styled/StyledOutlinedInput';
+import {
+    DATE_FORMAT_MASK,
+    DATE_REGEX,
+    ESC,
+    DATE_FORMAT,
+    NUMERIC_TYPE,
+    ENTER,
+    DELETE,
+    WARNING_COLOUR
+} from '../../constants';
+import {
+    isValidChar,
+    isNumPad,
+    markCellIsEditing,
+    removeCellIsEditing,
+    isValidDate,
+    setCaretPosition
+} from '../../helpers/helpers';
+
+const TextMaskWithInputMask = props => (
+    <InputMask {...props} mask={DATE_FORMAT_MASK} style={{ textOverflow: 'ellipsis' }} />
+);
 
 class DataTableDateEditor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editing: false,
+            showCalendar: false,
+            enterEditing: false
+        };
+    }
+
+    isWithinAcceptedDateRange = value => {
+        const { range: { from, to } = {} } = this.props;
+        const fromDate = from && isValidDate(from) ? new Date(from) : null;
+        const toDate = to && isValidDate(to) ? new Date(to) : null;
+        if (fromDate && toDate) {
+            const date = new Date(value);
+            return fromDate <= date && date <= toDate;
+        }
+        return true;
+    };
+
+    canAcceptValue = value =>
+        value && isValidDate(value) && DATE_REGEX.test(value) && this.isWithinAcceptedDateRange(value);
+
+    getAnchorElement = () => {
+        const { id } = this.props;
+        return document.getElementById(id);
+    };
+
+    enterEditMode = () => {
+        logger.debug('DataTableDateEditor enterEditMode');
+        const { id } = this.props;
+        this.setState({
+            editing: true,
+            enterEditing: true
+        });
+        markCellIsEditing(id);
+    };
+
+    exitEditMode = cancel => {
+        logger.debug('DataTableDateEditor exitEditMode');
+        const { id } = this.props;
+        this.setState({
+            editing: false
+        });
+        if (cancel) {
+            this.onCancel();
+        } else {
+            this.onCommit();
+        }
+
+        removeCellIsEditing(id);
+    };
+
+    commitChange = () => {
+        logger.debug('DataTableDateEditor commitChange');
+        const { editing, value } = this.props;
+        if (editing) {
+            const cancel = !this.canAcceptValue(value);
+            this.exitEditMode(cancel);
+        }
+    };
+
+    cancelChange = () => {
+        logger.debug('DataTableDateEditor cancelChange');
+        this.exitEditMode(true);
+    };
+
+    handleCalendarChange = (date, _) => {
+        logger.debug('DataTableDateEditor handleCalendarChange');
+        const { row, field, onCellChange } = this.props;
+        this.setState({
+            editing: false,
+            showCalendar: false
+        });
+        onCellChange(format(date, DATE_FORMAT), row, field, true);
+    };
+
+    handleKeyPress = () => {
+        logger.debug('DataTableDateEditor handleKeyPress');
+        const { enterEditing } = this.state;
+        const { id } = this.props;
+        if (enterEditing) {
+            setCaretPosition(document.getElementById(id), 0);
+            this.setState({
+                enterEditing: false
+            });
+        }
+    };
+
+    handleChange = event => {
+        logger.debug('DataTableDateEditor handleChange');
+        const { row, column, onCellChange } = this.props;
+        onCellChange(event.target.value, row, column.field);
+    };
+
+    handleKeyDown = e => {
+        logger.debug('DataTableDateEditor handleKeyDown');
+        const { column, onCellChange, row, value } = this.props;
+        const {
+            rich: { editable = false },
+            clearable = false
+        } = column;
+        const { editing } = this.state;
+        if (!editable) {
+            return;
+        }
+        if (e.keyCode === ESC) {
+            this.cancelChange();
+        }
+        if (!editing && clearable && value && e.keyCode === DELETE) {
+            onCellChange('', row, column.field, true);
+        }
+        const keyCode = isNumPad(e.keyCode) ? e.keyCode - 48 : e.keyCode;
+        if (!editing && e.keyCode !== DELETE && isValidChar(String.fromCharCode(keyCode), NUMERIC_TYPE)) {
+            this.enterEditMode();
+        }
+        if (editing && e.keyCode === ENTER) {
+            this.commitChange();
+        }
+    };
+
+    handleShowCalendar = () => {
+        logger.debug('DataTableDateEditor handleCalendar');
+        this.setState({
+            showCalendar: true
+        });
+    };
+
+    handleClose = () => {
+        logger.debug('DataTableDateEditor handleClose');
+        this.setState({
+            showCalendar: false
+        });
+    };
+
+    handleDoubleClick = () => {
+        logger.debug('DataTableDateEditor handleDoubleClick');
+        const { editing } = this.state;
+        if (!editing) {
+            this.enterEditMode();
+        }
+    };
+
+    handleBlur = () => {
+        logger.debug('DataTableDateEditor handleBlur');
+        const { onBlur, value } = this.props;
+        const cancel = !this.canAcceptValue(value);
+        if (cancel) {
+            this.cancelChange();
+        }
+        onBlur();
+    };
+
+    handleFocus = () => {
+        logger.debug('DataTableDateEditor handleFocus');
+        const { onActivateEditor } = this.props;
+        onActivateEditor();
+    };
+
     render() {
-        const { id, inputRef, onBlur } = this.props;
+        const { editing, showCalendar } = this.state;
+        const { id, inputRef, warning, error, column, value } = this.props;
+        const {
+            rich: {
+                editable = false,
+                date: { format: formatString }
+            }
+        } = column;
+        const stylingProps = {
+            style: {}
+        };
+        const inputProps = {
+            readOnly: !editing || !editable
+        };
+
+        if (warning && !error) {
+            stylingProps.style = {
+                ...stylingProps.style,
+                backgroundColor: WARNING_COLOUR
+            };
+        }
         return (
-            <StyledOutlinedInput
-                id={id}
-                onBlur={onBlur}
-                variant="outlined"
-                inputRef={inputRef}
-                inputProps={{ autoComplete: 'disabled' }}
-            />
+            <div>
+                <StyledFormControl>
+                    <StyledOutlinedInput
+                        id={id}
+                        error={!!error}
+                        title={error || warning || value}
+                        value={!editing && isValidDate(value) ? format(new Date(value), formatString) : value}
+                        onChange={this.handleChange}
+                        onKeyDown={this.handleKeyDown}
+                        onKeyPress={this.handleKeyPress}
+                        onDoubleClick={this.handleDoubleClick}
+                        onFocus={this.handleFocus}
+                        onBlur={this.handleBlur}
+                        inputRef={inputRef}
+                        inputComponent={TextMaskWithInputMask}
+                        inputProps={{ ...inputProps, ...stylingProps }}
+                        endAdornment={(
+                            <InputAdornment position="end">
+                                <IconButton onClick={this.handleShowCalendar}>
+                                    <EventIcon />
+                                </IconButton>
+                            </InputAdornment>
+                          )}
+                    />
+                </StyledFormControl>
+                <Popover
+                    open={showCalendar}
+                    anchorEl={this.getAnchorElement()}
+                    onClose={this.handleClose}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center'
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center'
+                    }}>
+                    <Calendar
+                        date={isValidDate(value) ? new Date(value) : new Date()}
+                        onChange={this.handleCalendarChange}
+                        allowKeyboardControl
+                    />
+                </Popover>
+            </div>
         );
     }
 }
---
+++ src/DataTable/styled/StyledFormControl.js
@@ -0,0 +1,13 @@
+import FormControl from '@material-ui/core/FormControl';
+import styled from 'styled-components';
+
+const StyledFormControl = styled(FormControl)`
+    .MuiOutlinedInput-root {
+        border-radius: 0px;
+    }
+    .MuiOutlinedInput-adornedEnd {
+        padding-right: 0px;
+    }
+`;
+
+export default StyledFormControl;
